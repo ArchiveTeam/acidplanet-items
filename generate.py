0459fb1d20dc2f07c8c6b5452ca696b07e54ef71
Arkiver2 <arkiver@protonmail.com>
2018-05-27 13:20:58 +0200

    Split item list up

--- generate.py
+++ generate.py
@@ -4,7 +4,11 @@ import os
 with open(os.path.join('original', 'audiourls.txt'), 'r') as f:
     data = [s.split('/', 4)[-1].strip() for s in f if len(s) > 0]
 
-with open('0000_audiourls', 'w') as f:
-    f.write('\n'.join(['s3urls:{}'.format(';'.join(data[i:i+5]))
-                       for i in range(0, len(data), 5)]))
+
+items = ['s3urls:{}'.format(';'.join(data[i:i+5]))
+         for i in range(0, len(data), 5)]
+
+for i in range(0, len(items), 50000):
+    with open('{}_audiourls.txt'.format(str(i/50000).zfill(4)), 'w') as f:
+        f.write('\n'.join(items[i:i+50000]))
 
